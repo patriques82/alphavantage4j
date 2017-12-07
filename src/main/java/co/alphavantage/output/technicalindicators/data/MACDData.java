e423b5b57c80de2e983f4c2381917ddea90b40e0
jesse <jesse.mazzie@agile5technologies.com>
2017-12-07 10:35:56 -0500

    Completed MACD and MACDData class implementations.

--- src/main/java/co/alphavantage/output/technicalindicators/data/MACDData.java
+++ src/main/java/co/alphavantage/output/technicalindicators/data/MACDData.java
@@ -1,5 +1,21 @@
 package co.alphavantage.output.technicalindicators.data;
 
-public class MACDData {
+import org.joda.time.DateTime;
 
+public class MACDData {
+	private final DateTime dateTime;
+	private final double macd;
+	
+	public MACDData(DateTime dateTime, double macd) {
+		this.dateTime = dateTime;
+		this.macd = macd;
+	}
+	
+	public DateTime getDateTime() {
+		return dateTime;
+	}
+	
+	public double getMacd() {
+		return macd;
+	}
 }
