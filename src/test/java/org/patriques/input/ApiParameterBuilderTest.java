61d4e32eb500a45eb36e797f228f98d74223a36d
Patrik Nygren <patriknygren82@gmail.com>
2017-12-20 16:12:49 +0100

    added test to ApiParameterBuilder

---
+++ src/test/java/org/patriques/input/ApiParameterBuilderTest.java
@@ -0,0 +1,64 @@
+package org.patriques.input;
+
+import org.junit.Test;
+import org.patriques.input.digitalcurrencies.Market;
+import org.patriques.input.exchange.FromCurrency;
+import org.patriques.input.exchange.ToCurrency;
+import org.patriques.input.technicalindicators.Acceleration;
+import org.patriques.input.technicalindicators.FastDMaType;
+import org.patriques.input.technicalindicators.FastDPeriod;
+import org.patriques.input.technicalindicators.Interval;
+import org.patriques.input.timeseries.OutputSize;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ApiParameterBuilderTest {
+
+  @Test
+  public void digitalCurrencies() {
+    ApiParameterBuilder apiParameterBuilder = new ApiParameterBuilder();
+    apiParameterBuilder.append(Function.DIGITAL_CURRENCY_DAILY);
+    apiParameterBuilder.append(Market.ZWL);
+
+    String url = apiParameterBuilder.getUrl();
+
+    assertThat(url, equalTo("&function=DIGITAL_CURRENCY_DAILY&market=ZWL"));
+  }
+
+  @Test
+  public void exchange() {
+    ApiParameterBuilder apiParameterBuilder = new ApiParameterBuilder();
+    apiParameterBuilder.append(new FromCurrency("BTC"));
+    apiParameterBuilder.append(new ToCurrency("USD"));
+
+    String url = apiParameterBuilder.getUrl();
+
+    assertThat(url, equalTo("&from_currency=BTC&to_currency=USD"));
+  }
+
+  @Test
+  public void technicalIndicators() {
+    ApiParameterBuilder apiParameterBuilder = new ApiParameterBuilder();
+    apiParameterBuilder.append(Acceleration.of(1.0f));
+    apiParameterBuilder.append(FastDMaType.SMA);
+    apiParameterBuilder.append(FastDPeriod.of(2));
+
+    String url = apiParameterBuilder.getUrl();
+
+    assertThat(url, equalTo("&acceleration=1,00&fastdmatype=0&fastdperiod=2"));
+  }
+
+  @Test
+  public void timeSeries() {
+    ApiParameterBuilder apiParameterBuilder = new ApiParameterBuilder();
+    apiParameterBuilder.append(new Symbol("DUMMY"));
+    apiParameterBuilder.append(Function.TIME_SERIES_DAILY);
+    apiParameterBuilder.append(Interval.FIFTEEN_MIN);
+    apiParameterBuilder.append(OutputSize.FULL);
+
+    String url = apiParameterBuilder.getUrl();
+
+    assertThat(url, equalTo("&symbol=DUMMY&function=TIME_SERIES_DAILY&interval=15min&outputsize=full"));
+  }
+}
